ae070ef74b606921f41713fdd11e61be602dbcb0
Alan Lu <lufan.angel@gmail.com>
2022-08-06 10:55:42 +0800

    first commit

--- cloudflare/dns.tf
+++ cloudflare/dns.tf
@@ -1,7 +1,7 @@
 resource "cloudflare_record" "default" {
   zone_id  = var.zone_id
   for_each = var.a_record_info
-  name     = tostring(each.key)
+  name     = each.key
   value    = each.value
   type     = var.type
   ttl      = var.ttl
